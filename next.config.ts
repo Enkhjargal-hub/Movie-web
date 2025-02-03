835a2ba696eaced5d38a19173bd83bc4d05b9376
Enkhjargal-hub <Joloenja@gmail.com>
2025-02-03 10:22:41 +0800

    j

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "image.tdmb.org",
+      },
+    ],
+  },
+  env: {
+    TDMB_BASE_URL: process.env.TDMB_BASE_URL || "",
+    TDMB_API_TOKEN: process.env.TDMB_API_TOKEN || "",
+    TDMB_IMAGE_SERVICE_URL: process.env.TDMB_IMAGE_SERVICE_URL || "",
+  },
 };
 
 export default nextConfig;
+
