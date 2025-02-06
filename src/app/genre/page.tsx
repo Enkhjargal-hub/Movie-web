f61601ca698433e66dd0587399a83f9c9a8ed9b9
Enkhjargal-hub <Joloenja@gmail.com>
2025-02-06 11:29:36 +0800

    j

--- src/app/genre/page.tsx
+++ src/app/genre/page.tsx
@@ -30,17 +30,52 @@ const Genres = () => {
             console.log("Axios error:", error);
         });
     };
+
     useEffect(()=> {
         getGenresList();
     },[]);
+
     const handleGenreSelection=(genreId:string) => () => {
+        const updatedGenres=selectedGenreIds.includes(genreId)
+        ?setSelectedGenreIds(selectedGenreIds.filter((item)=> item!==genreId))
+        :setSelectedGenreIds(...selectedGenreIds, genreId);
+
         const queryParams = new URLSearchParams();
+        queryParams.set("genreId", updatedGenres.join(","));
+
+
 
         setSelectedGenreIds([...selectedGenreIds,genreId]);
         push(newPath);
     };
 
     return (
-        <div className="w-full grid grid-cols-3 gap-2"></div>
-    )
-}
+        <div className="w-full grid grid-cols-3 gap-2">
+            <div className="col-span-1 space-x-1">
+                {genres.length > 0 &&
+                genres?.map((item)=> {
+                    const genreId = item.id.toString();
+                    const isSelected = selectedGenreIds.includes(genreId);
+                    return (
+                        <Badge
+                        onClick={handleGenreSelection(genreId)}
+                        variant="outline"
+                        key={item.name}
+                        className={`${
+                            isSelected
+                            ?"bg-black text-white dark:bg-white dark:text-black"
+                            : ""
+                        } rounded-full cursor-pointer`}
+                        >
+                            {item.name}
+                        </Badge>
+                    );
+                })}
+            </div>
+            <Separator orientation="vertical"/>
+            <div className="col-span-2">Movies</div>
+        </div>
+    );
+};
+
+export default Genres;
