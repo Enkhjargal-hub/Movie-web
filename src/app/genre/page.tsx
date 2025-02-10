13674f82d5b001d4fb4c0a68bcfe6dcd873f76eb
Enkhjargal-hub <Joloenja@gmail.com>
2025-02-10 11:28:21 +0800

    h

--- src/app/genre/page.tsx
+++ src/app/genre/page.tsx
@@ -1,112 +1,122 @@
-"use client"
+"use client";
+
 import { Separator } from "@/components/ui/separator";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {Badge} from "@/components/ui/badge";
-import {useRouter} from "next/navigation"
-import {useSearchParams} from "next/navigation";
-import { error } from "console";
-import { EqualApproximatelyIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const TMDB_BASE_URL= process.env.TMDB_BASE_URL;
-const TMDB_API_TOKEN=process.env.TMDB_API_TOKEN;
+const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
+const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
 
-type GenresType = {id:number; number:string};
+type GenresType = { id: number; name: string };
 
 type Movie = {
-    original_title: string;
+  id: number;
+  original_title: string;
 };
 
 const Genres = () => {
-    const {push} = useRouter();
-    const searchParams = useSearchParams();
-    const [genres, setGenres] = useState<GenresType[]>([]);
-    const [selectedGenreIds, setSelectedGenreIds] =useState<string[]>([]);
-    const searchedGenreId= searchParams.get("genresId")
-    const [movie, setMovies] = useState<Movie[]>([]);
-    searchParams.get("genresId");
+  const { push } = useRouter();
+  const searchParams = useSearchParams();
+  const [genres, setGenres] = useState<GenresType[]>([]);
+  const [selectedGenreIds, setSelectedGenreIds] = useState<string[]>([]);
+  const searchedGenreId = searchParams.get("genreId");
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+ 
+  const getGenresList = async () => {
+    try {
+      const response = await axios.get(`${TMDB_BASE_URL}/genre/movie/list?language=en`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${TMDB_API_TOKEN}`,
+        },
+      });
+
+      setGenres(response.data.genres);
+    } catch (error) {
+      console.error("Axios error:", error);
+    }
+  };
 
 
-    const getMoviesByGenres = async () => {
-    
-    const genreIds = searchParams.get("genreIds");
-    await axios
-    .get(
+  const getMoviesByGenres = async () => {
+    const genreIds = searchParams.get("genreId");
+    if (!genreIds) return;
+
+    try {
+      const response = await axios.get(
         `${TMDB_BASE_URL}/discover/movie?language=en&with_genres=${genreIds}&page=1`,
         {
-            headers: {
-                "Content-Type":"application/json",
-                Authorization: `Bearer ${TMDB_API_TOKEN}`,
-            },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${TMDB_API_TOKEN}`,
+          },
         }
-    )
-    .then((response)=>{
-        console.log(response);
-        
-        setMovies(response.data.genres);
-    })
-    .catch((error)=> {
-        console.log("Axios error:", error);
-    });
-    };
+      );
 
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error("Axios error:", error);
+    }
+  };
 
-    useEffect(()=> {
-        getGenresList();
-    },[]);
+  useEffect(() => {
+    getGenresList();
+  }, []);
 
-    useEffect(()=> {
-        getMoviesByGenres();
-    },[searchedGenreId]);
+  useEffect(() => {
+    getMoviesByGenres();
+  }, [searchedGenreId]);
 
-    const handleGenreSelection=(genreId:string) => () => {
-        const updatedGenres=selectedGenreIds.includes(genreId)
-        ?setSelectedGenreIds(selectedGenreIds.filter((item)=> item!==genreId))
-        :setSelectedGenreIds(...selectedGenreIds, genreId);
+  const handleGenreSelection = (genreId: string) => () => {
+    const updatedGenres = selectedGenreIds.includes(genreId)
+      ? selectedGenreIds.filter((item) => item !== genreId)
+      : [...selectedGenreIds, genreId];
 
-        setSelectedGenreIds(updatedGenres);
+    setSelectedGenreIds(updatedGenres);
 
-        const queryParams = new URLSearchParams();
-        queryParams.set("genreId", updatedGenres.join(","));
-        const newPath = queryParams.toString()
-    
+    const queryParams = new URLSearchParams();
+    queryParams.set("genreId", updatedGenres.join(","));
+    push(`/genres?${queryParams.toString()}`);
+  };
 
-        setSelectedGenreIds([...selectedGenreIds,genreId]);
-        push(`/genres?${newPath}`);
-    };
-
-    return (
-        <div className="w-full flex gap-2 h-screen">
-            <div className="w-1/3 space-x-1">
-                {genres.length > 0 &&
-                genres?.map((item)=> {
-                    const genreId = item.id.toString();
-                    const isSelected = selectedGenreIds.includes(genreId);
-                    return (
-                        <Badge
-                        onClick={handleGenreSelection(genreId)}
-                        variant="outline"
-                        key={item.name}
-                        className={`${
-                            isSelected
-                            ?"bg-black text-white dark:bg-white dark:text-black"
-                            : ""
-                        } rounded-full cursor-pointer`}
-                        >
-                            {item.name}
-                        </Badge>
-                    );
-                })}
+  return (
+    <div className="w-full flex gap-2 h-screen p-4">
+      <div className="w-1/3 space-x-1">
+        {genres.length > 0 &&
+          genres.map((item) => {
+            const genreId = item.id.toString();
+            const isSelected = selectedGenreIds.includes(genreId);
+            return (
+              <Badge
+                onClick={handleGenreSelection(genreId)}
+                variant="outline"
+                key={item.id}
+                className={`cursor-pointer rounded-full ${
+                  isSelected ? "bg-black text-white dark:bg-white dark:text-black" : ""
+                }`}
+              >
+                {item.name}
+              </Badge>
+            );
+          })}
+      </div>
+      <Separator orientation="vertical" />
+      <div className="w-2/3">
+        {movies.length > 0 ? (
+          movies.map((item) => (
+            <div key={item.id} className="text-white">
+              {item.original_title}
             </div>
-            <Separator orientation="vertical"/>
-            <div className="col-span-2">
-                {movie?.length> 0 && 
-                movie.map((item)=> {
-                    return <div key= {item.original_title}>{item.original_title}</div>
-                })}
-            </div>
-        </div>
-    );
+          ))
+        ) : (
+          <p className="text-gray-400">No movies found.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Genres;
